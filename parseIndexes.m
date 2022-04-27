ba69c53d7fc7c036250ee5e1b9a4d787129c14ac
Jeremi Chabros <jjc@Jeremis-MacBook-Air.local>
2022-04-27 21:13:07 +0100

    sporg selection code

---
+++ parseIndexes.m
@@ -0,0 +1,15 @@
+function parseIndexes(pos)
+for i = 1:size(pos,1)
+
+    xpos = find(pos(i,1)==X);
+    xpos = xpos(1);
+
+    ypos = find(pos(i,2)==Y);
+    ypos = ypos(1);
+
+    [~,column] = ind2sub(8,xpos);
+    [row,~] = ind2sub(8,ypos);
+
+    electrode_id(i) = 10*column + row;
+end
+end
